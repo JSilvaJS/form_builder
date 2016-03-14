311964fe0cf963ceae5704cc9e5f5451510022ad
JONATHAN SILVA <silva.jon86@gmail.com>
2016-03-14 19:58:11 -0400

    finished JS & HTML, now working on SASS then FINITO!

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,57 @@
 // Javascript Entry Point
+
+import $ from 'jquery';
+import data from './form_data';
+import { text, select, textArea } from './template';
+
+// var inputInfoToTemplate = {
+// 	'label':firstName
+// }
+
+
+// data.forEach(function(inputInfo){
+// $('.content_zone').append(text(inputInfo));
+
+// });
+
+
+
+var dataTypeToTemplate = {
+	'text': text,
+	'email':text,
+	'select': select,
+	'textarea':textArea,
+	'tel':text
+}
+
+data.forEach(function(inputInfo) {
+	var template = dataTypeToTemplate[inputInfo.type];
+	var html = template(inputInfo);
+	$('.content_zone').append(html);
+});
+
+
+
+
+
+
+
+// import $ from 'jquery';
+
+// import users from './users';
+
+// import { manager, employee, customer, cool } from './templates';
+
+// var $app = $('.app');
+
+// var userTypeToTemplate = {
+//   'Manager': manager,
+//   'Employee': employee,
+//   'Customer': customer
+// }
+
+// users.forEach(function(user) {
+//   var template = userTypeToTemplate[user.type];
+//   var html = template(user);
+//   $app.append( html );
+// });
---
+++ src/js/template.js
@@ -0,0 +1,104 @@
+
+
+export function text(inputInfo) {
+	return `
+	<div class="text_div">
+		<input type="${inputInfo.type}" id="${inputInfo.id}" placeholder="${inputInfo.label}">
+	</div>
+	`;
+}
+
+export function select(inputInfo) {
+	var ops = inputInfo.options;
+
+	var opsOptions = ops.map(function(op){
+		return `<option>${op.label}</option>`;
+	});
+
+
+	opsOptions = opsOptions.join('')
+
+	return `
+	<div class="select_div">
+	  	<label for="${inputInfo.id}""><label>
+  		<select id="${inputInfo.id}">
+  			${opsOptions}
+  		</select>
+  	</div>
+  		`;
+};
+
+
+export function textArea (inputInfo) {
+	return `
+	<div class="textArea_div">
+		<textarea type="${inputInfo.type}" id="${inputInfo.id}" placeholder="${inputInfo.label}"></textarea>
+	</div>
+	`;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// export function manager(user) {
+  
+//   var subs = user.subordinates;
+
+//   var subsHtml = subs.map(function(sub) {
+//     return `<li>${sub}</li>`;
+//   });
+
+//   subsHtml = subsHtml.join('');
+
+//   return `
+//     <div class="user manager">
+//       <div class="first-name">${user.firstName}</div>
+//       <div class="last-name">${user.lastName}</div>
+//       <div class="employee-id">${user.employeeId}</div>
+//       <div class="role">${user.role}</div>
+//       <ul class="subordinates">
+//         ${subsHtml}
+//       </ul>
+//     </div>
+//   `;
+// }
+
+// export function employee(user) {
+//   return `
+//     <div class="user employee">
+//       <div class="first-name">${user.firstName}</div>
+//       <div class="last-name">${user.lastName}</div>
+//       <div class="employee-id">${user.employeeId}</div>
+//       <div class="role">${user.role}</div>
+//       <div class="sales-record">${user.salesRecord}</div>
+//     </div>
+//   `;
+// }
+
+// export function customer(user) {
+//   var purs = user.purchaseHistory;
+
+//   var pursHtml = purs.map(function(pur) {
+//     return `<li>${pur}</li>`;
+//   });
+
+//   pursHtml = pursHtml.join('');
+
+//   return `
+//     <div class="user customer">
+//       <div class="first-name">${user.firstName}</div>
+//       <div class="last-name">${user.lastName}</div>
+//       <ul class="purchase-history">
+//         ${pursHtml}
+//       </ul>
+//     </div>
+//   `;
+// }
